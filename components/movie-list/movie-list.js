5465d84e38c411a4690a3efda28977506a661404
Will Morrissey <wtm.41691@gmail.com>
2019-05-03 15:14:57 -0400

    Adjusted movie list component - Still needs work

--- components/movie-list/movie-list.js
+++ components/movie-list/movie-list.js
@@ -1,20 +1,62 @@
 "use strict";
 
-function MovieListController(MovieAppService) { 
+function MovieListController(MovieAppService) {
+
     const ctrl = this;
     const service = MovieAppService;
+    ctrl.movieList = [];
+
+    ctrl.getMovies = () => {
+        return $q(function(resolve, reject) {
+    
+        MovieAppService.getMovies()
+          .then ( (response) => {
+            console.log(response);
+             
+              let children = response.data.data.children; //Adjust for proper API return
+      
+                children.forEach( function(child, index) {
+                  let movieObj = {
+                    title: child.data.title,
+                    poster: child.data.thumbnail, //Change thumbnail to appropraite return from API
+                    description: child.data.permalink  // Change permalink to appropraite return from API 
+                  }
+                  
+                  ctrl.movieList.push(movieObj);
+      
+                  if ( index === (children.length -1) ){
+                    resolve();
+                  }
+      
+                })
+            });
+        });
+      }
+      
+        ctrl.getMovies()
+       
+    }
 
     
-    //Working on populating page
+    
 
-}
+
 
 
 angular
 .module('MovieApp')  
 .component('movieList', {
     template: `
-
+    <div ng-repeat="post in $ctrl.movieList">
+    <div id="box-container">
+    <div class = "title">
+      <h2>{{post.title}}</h2>
+    </div>
+    <div class = "image">
+      <img ng-src="{{post.poster}}"></img>
+    </div>
+    </div>
+  </div>
 
         `,
     controller: MovieListController
