53dbadfbd5be4b149983b20bf99a176108b8c7a5
Derek <derekdenhartigh@gmail.com>
2019-05-06 12:14:23 -0400

    cloned the movie-list styling over to the watchlist component and the site is functional, probably at minimally viable product stage

--- components/movie-list/movie-list.js
+++ components/movie-list/movie-list.js
@@ -80,8 +80,8 @@ angular
         <div class="movie-post" ng-repeat="movie in $ctrl.movieList">
             <div class="title-container">
                 <h1 class="movie-title title" ng-click="show=!show">{{movie.title}}</h1>
-                <div id="spacer"></div>
-                <div id="star-container">
+                <div class="spacer"></div>
+                <div class="star-container">
                     <i class="material-icons star" ng-hide="movie.starred" ng-click="$ctrl.watchlistEditor(movie)">star_border</i>
                     <i class="material-icons star" ng-show="movie.starred" ng-click="$ctrl.watchlistEditor(movie)">star</i>
                 </div>
--- components/watch-list/watch-list.js
+++ components/watch-list/watch-list.js
@@ -7,14 +7,24 @@ function WatchListController(MovieAppService) {
     
 }
 
-
 angular
 .module('MovieApp')  
 .component('watchList', {
     template: `
     <!--Watchlist-->
-    <div class="watchlist-box" ng-repeat="movie in $ctrl.watchlistArray">
-        <!--this will be a copy/paste job from movie-list but built from an alternate array-->
+    <div id="watchlist-container">
+        <div class="movie-post" ng-repeat="movie in $ctrl.watchlistArray">
+        <div class="title-container">
+            <h1 class="movie-title title" ng-click="show=!show">{{movie.title}}</h1>
+            <div class="spacer"></div>
+            <div class="star-container">
+                <i class="material-icons star" ng-hide="movie.starred" ng-click="$ctrl.watchlistEditor(movie)">star_border</i>
+                <i class="material-icons star" ng-show="movie.starred" ng-click="$ctrl.watchlistEditor(movie)">star</i>
+            </div>
+        </div>
+        <img class="movie-poster image" alt="movie poster" ng-src="{{movie.poster}}" ng-click="show=!show"></img>
+        <p class ="movie-description description" ng-hide="!show">Synopsis:\n{{movie.description}}</p>
+        </div>
     </div>
         `,
     controller: WatchListController
