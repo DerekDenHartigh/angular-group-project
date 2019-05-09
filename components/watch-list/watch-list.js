20a319d4964b17b4e8308c73d18dc185a677918f
Derek <derekdenhartigh@gmail.com>
2019-05-09 08:04:01 -0400

    fixed watchlist styling

--- components/watch-list/watch-list.js
+++ components/watch-list/watch-list.js
@@ -19,15 +19,16 @@ angular
     template: `
     <!--Watchlist-->
     <div id="watchlist-container">
-        <div class="movie-post watchlist-box" ng-repeat="movie in $ctrl.watchlistArray">
+        <div class="movie-post" ng-repeat="movie in $ctrl.watchlistArray">
         <div class="title-container">
             <h1 class="movie-title title" ng-click="show=!show">{{movie.title}}</h1>
             <div class="spacer"></div>
-            <div class="star-container" id="detailed-star-container">
+            <div class="star-container">
                 <i class="material-icons star" ng-hide="movie.starred" ng-click="$ctrl.watchlistEditor(movie)">star_border</i>
                 <i class="material-icons star" ng-show="movie.starred" ng-click="$ctrl.watchlistEditor(movie)">star</i>
             </div>
         </div>
+
         <img class="movie-poster image" alt="movie poster" ng-src="{{movie.poster}}" ng-click="show=!show"></img>
         <p class ="movie-description description" ng-hide="!show">Synopsis:\n{{movie.description}}</p>
         <a class = "more-info" href="#!/moreInfo" ng-click="$ctrl.infoFunction(movie)">More Info...</a>
