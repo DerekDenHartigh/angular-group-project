1b74b2d458a9a94111d99032861b88a050633fc3
Derek <derekdenhartigh@gmail.com>
2019-05-10 06:29:20 -0400

    first commit, this branch aims to add a search query drawing from an alternate branch of the movie db api

--- movie-app-service.js
+++ movie-app-service.js
@@ -24,7 +24,7 @@ function MovieAppService($http, $q) {
 
     service.movieList = [];
 
-/* API interactions */
+/* Discover API interactions */
 
     service.callTheMovieDbApi = () => {
         return $q(function(resolve, reject){
@@ -93,6 +93,77 @@ service.getMovies = () => {
         });
     });
   }
+
+
+/* Search API interactions - not modified yet */
+
+service.callTheMovieDbApi = () => {
+    return $q(function(resolve, reject){
+        console.log("all the params in callTheMovieDbApi:");
+        console.log(service.api_key, service.pageNumber, service.vote_averageGreaterThanOrEqual, service.earliestReleaseDate, service.latestReleaseDate, 
+            service.genreSelection, service.genresNotWanted, service.runTimeLessThanOrEqual);
+        $http.get('https://api.themoviedb.org/3/discover/movie', {
+        params: {
+            api_key: service.api_key,
+            language: "en-US",
+            sort_by: "popularity.desc",
+            include_adult: false,
+            include_video: false,
+            page: service.pageNumber,
+            'release_date.gte': service.earliestReleaseDate,
+            'release_date.lte': service.latestReleaseDate,
+            // 'with_genres': service.genreSelection,  // perhaps having both params breaks it?  i'm not sure why..
+            'without_genres': service.genresNotWanted,
+            'with_runtime.gte': service.runTimeGreaterThanOrEqual,
+            'with_runtime.lte': service.runTimeLessThanOrEqual,
+            'vote_average.gte': service.vote_averageGreaterThanOrEqual,
+            'vote_average.lte': service.vote_averageLessThanOrEqual
+        }
+    })
+        .then( (response)=>{
+            response.data.results.forEach((movie)=>{ // this is to add starred boolean for watchlist usage
+                movie.starred = false;
+            });
+            service.responseData = response.data; // saves data to service
+            resolve(response.data);  // the return of a promise
+        })
+    }
+)
+};
+
+service.getMovies = () => {
+return $q(function(resolve, reject) {
+
+service.callTheMovieDbApi()
+  .then ( (response) => {
+    console.log("response of callTheMovieDbApi:");
+    console.log(response);
+    let movies=[];
+
+    service.pageLimitFunction() // uses service.responseData to write page limit        
+      let children = response.results; //Adjust for proper API return
+        children.forEach( function(child, index) {
+            let isWatchlisted = ( service.isWatchlisted(child.id) !== false );
+          let movieObj = {
+            title: child.title,
+            poster: `https://image.tmdb.org/t/p/w185/` + child.poster_path, //Change thumbnail to appropraite return from API
+            description: child.overview,  // Change permalink to appropraite return from API 
+            backdrop: `https://image.tmdb.org/t/p/original/` + child.backdrop_path,
+            avgVote: child.vote_average,
+            releaseDate: child.release_date,
+            genres: child.genre_ids, // array of genre id #s
+            id: child.id,
+            starred: isWatchlisted // if movie ID is in the watchlistArray, it returns a number, a number !== false, so this is "true", if it returns false, false!==false is "false".
+          }
+          movies.push(movieObj);
+          if ( index === (children.length - 1) ){
+              service.movieList = movies;
+            resolve();
+          }
+        })
+    });
+});
+}
 
 /* Genre Land */
     service.genreOptionArray = [];  // to populate our genre selections (check & X boxes for include/exclued)
